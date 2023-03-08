7802ffe97cfa8965408036c8c9b8f0fda2939eb7
jumst.code@gmail.com <jumiyoon@Jumis-MacBook-Pro.local>
2023-03-07 22:32:55 -0800

    commit tree 94dc477bff0207ebb4d398af1f5af29ba633d7d2

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,45 @@
+require 'pry'
+
 class Triangle
-  # write code here
+  
+  attr_reader :side1, :side2, :side3 
+
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3 
+
+    sorted = [side1, side2, side3].sort
+
+    if side1 > 0 && side2 >0 && side3 >0
+      nil
+    end
+
+    unless sorted[0]+sorted[1] > sorted[2] 
+      raise TriangleError
+    end
+
+
+
+  end
+
+
+  def kind
+    if side1 == side2 && side2 == side3
+      :equilateral
+    elsif side1 == side2 || side2 == side3 || side1 == side3
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+    def message
+      "Not a trinagle!"
+    end
+  end
+
+
+    
 end
